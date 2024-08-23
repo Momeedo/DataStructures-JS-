fd885bac1de24036103475752e8483cfacf1203c
Mohamed Turki <mohamed.turki@esprit.tn>
2024-08-23 19:49:35 +0100

    LL Insert

--- LinkedList/Demo.js
+++ LinkedList/Demo.js
@@ -106,7 +106,21 @@ class LinkedList {
   }
 
   //Creates a new Node & inserts it
-  insert(value) {}
+  insert(index, value) {
+    if (index < 0 || index >= this.length) return false;
+
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.push(value);
+
+    const newNode = new Node(value);
+    const temp = this.get(index - 1);
+
+    newNode.next = temp.next;
+    temp.next = newNode;
+
+    this.length++;
+    return true;
+  }
 }
 
 let myLinkedList = new LinkedList(7);
