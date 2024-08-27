436d6483660475c611b5599c3a1752ba5f0174f2
Mohamed Turki <mohamed.turki@esprit.tn>
2024-08-27 01:14:33 +0100

    Queues

---
+++ Queues/Demo.js
@@ -0,0 +1,62 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.first = newNode;
+    this.last = newNode;
+    this.length = 1;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  dequeue() {
+    if (this.length === 0) return undefined;
+
+    let temp = this.first;
+    if (this.length === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+      temp.next = null;
+    }
+
+    this.length--;
+
+    return temp;
+  }
+}
+
+let myQueue = new Queue(1);
+console.log(myQueue);
+
+console.log("\n\n----------------");
+console.log("----------------\n\n");
+
+myQueue.enqueue(2);
+myQueue.enqueue(3);
+console.log(myQueue);
+
+console.log("\n\n----------------");
+console.log("----------------\n\n");
+
+myQueue.dequeue();
+console.log(myQueue);
