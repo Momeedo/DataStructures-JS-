e3a2241909ff9591649440b78f62055782a59626
Mohamed Turki <t.momeedo@gmail.com>
2024-09-01 13:00:08 +0100

    Refractor: Hash Tables Interview Question: Items in Common

--- Hash Tables/Exercice (Items in Common).js
+++ Hash Tables/Exercice (Items in Common).js
@@ -1,15 +1,53 @@
-function itemInCommon(arr1, arr2) {
-  let obj = {};
-  for (let i = 0; i < arr1.length; i++) {
-    obj[arr1[i]] = true;
-  }
-  for (let i = 0; i < arr2.length; i++) {
-    if (obj[arr2[i]]) return true;
-  }
-  return false;
+function itemInCommon(array1, array2) {
+    const myMap = new Map();
+    
+    for (let i of array1) {
+        myMap.set(i, true);
+    }
+    
+    for (let j of array2) {
+        if (myMap.has(j)) return true;
+    }
+    
+    return false;
 }
 
-let array1 = [1, 3, 5];
-let array2 = [2, 4, 5];
+// ---------------
+// One Common Item
+// ---------------
+console.log("One Common Item:");
+console.log("Input: [1, 3, 5], [2, 4, 5]");
+console.log("Output: ", itemInCommon([1, 3, 5], [2, 4, 5]));
+console.log("---------------");
 
-console.log(itemInCommon(array1, array2));
+// ---------------
+// No Common Items
+// ---------------
+console.log("No Common Items:");
+console.log("Input: [1, 3, 5], [2, 4, 6]");
+console.log("Output: ", itemInCommon([1, 3, 5], [2, 4, 6]));
+console.log("---------------");
+
+// ---------------
+// Multiple Common Items
+// ---------------
+console.log("Multiple Common Items:");
+console.log("Input: [1, 2, 3], [2, 3, 4]");
+console.log("Output: ", itemInCommon([1, 2, 3], [2, 3, 4]));
+console.log("---------------");
+
+// ---------------
+// Empty Arrays
+// ---------------
+console.log("Empty Arrays:");
+console.log("Input: [], []");
+console.log("Output: ", itemInCommon([], []));
+console.log("---------------");
+
+// ---------------
+// One Empty Array
+// ---------------
+console.log("One Empty Array:");
+console.log("Input: [1, 2, 3], []");
+console.log("Output: ", itemInCommon([1, 2, 3], []));
+console.log("---------------");
