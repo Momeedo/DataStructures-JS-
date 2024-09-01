ff423c87b2a12df9e970f4cabb5316baed2d14fe
Mohamed Turki <t.momeedo@gmail.com>
2024-09-01 14:08:28 +0100

    Hash Tables Interview Question: Find First Unique Character

---
+++ Hash Tables/Exercice (First Unique Character).js
@@ -0,0 +1,62 @@
+function firstNonRepeatingChar(string) {
+  const charCounts = new Map();
+
+  for (let char in string) {
+    charCounts.set(char, (charCounts.get(char) || 0) + 1);
+  }
+
+  for (let [key, value] of charCounts.entries()) {
+    if (value === 1) {
+      return key;
+    }
+  }
+  return null;
+}
+
+// ---------------
+// All Unique
+// ---------------
+console.log("All Unique:");
+console.log("Input: 'abcde'");
+console.log("Output: ", JSON.stringify(firstNonRepeatingChar("abcde")));
+console.log("---------------");
+
+// ---------------
+// Some Duplicates
+// ---------------
+console.log("Some Duplicates:");
+console.log("Input: 'aabbccdef'");
+console.log("Output: ", JSON.stringify(firstNonRepeatingChar("aabbccdef")));
+console.log("---------------");
+
+// ---------------
+// All Duplicates
+// ---------------
+console.log("All Duplicates:");
+console.log("Input: 'aabbcc'");
+console.log("Output: ", JSON.stringify(firstNonRepeatingChar("aabbcc")));
+console.log("---------------");
+
+// ---------------
+// Empty String
+// ---------------
+console.log("Empty String:");
+console.log("Input: ''");
+console.log("Output: ", JSON.stringify(firstNonRepeatingChar("")));
+console.log("---------------");
+
+// ---------------
+// Single Character
+// ---------------
+console.log("Single Character:");
+console.log("Input: 'a'");
+console.log("Output: ", JSON.stringify(firstNonRepeatingChar("a")));
+console.log("---------------");
+
+// ---------------
+// Multiple Non-Repeating Chars
+// ---------------
+console.log("Multiple Non-Repeating Chars:");
+console.log("Input: 'abcdaef'");
+console.log("Output: ", JSON.stringify(firstNonRepeatingChar("abcdaef")));
+console.log("---------------");
