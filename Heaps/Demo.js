4fb2f814156dc0e00042e844b2ab5eced7fc6215
Mohamed Turki <t.momeedo@gmail.com>
2024-09-02 17:53:08 +0100

    Heaps: Remove (& sinkDown Helper)

--- Heaps/Demo.js
+++ Heaps/Demo.js
@@ -24,6 +24,28 @@ class Heap {
     ];
   }
 
+  #sinkDown(index) {
+    let maxIndex = index;
+    let size = this.#heap.length;
+    while (true) {
+      let leftIndex = this.#leftChild(index);
+      let rightIndex = this.#rightChild(index);
+
+      if (leftIndex < size && this.#heap[leftIndex] > this.#heap[maxIndex]) {
+        maxIndex = leftIndex;
+      }
+
+      if (rightIndex < size && this.#heap[rightIndex] > this.#heap[maxIndex]) {
+        maxIndex = rightIndex;
+      }
+
+      if (maxIndex !== index) {
+        this.#swap(index, maxIndex);
+        index = maxIndex;
+      }
+    }
+  }
+
   insert(value) {
     this.#heap.push(value);
     let current = this.#heap.length - 1;
@@ -34,6 +56,18 @@ class Heap {
       this.#swap(current, this.#parent(current));
       current = this.#parent(current);
     }
+  }
+
+  //Remove: Always remove the [0] then Pop the last item and move it to [0] then reorganize
+  remove() {
+    if (this.#heap.length === 0) return null;
+    if (this.#heap.length === 1) return this.#heap.pop();
+
+    const maxValue = this.#heap[0];
+    this.#heap[0] = this.#heap.pop();
+    this.#sinkDown(0);
+
+    return maxValue;
   }
 }
 
