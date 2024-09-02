722543ca84dc274a409428fceb004d5d3143b667
Mohamed Turki <t.momeedo@gmail.com>
2024-09-02 17:10:22 +0100

    Heaps: Helper Methods

---
+++ Heaps/Demo.js
@@ -0,0 +1,26 @@
+class Heap {
+  #heap = [];
+
+  getHeap() {
+    return [...this.#heap]; //Return a copy of #heap
+  }
+
+  #leftChild(index) {
+    return 2 * index + 1;
+  }
+
+  #rightChild(index) {
+    return 2 * index + 2;
+  }
+
+  #parent(index) {
+    return Math.floor((index - 1) / 2);
+  }
+
+  #swap(index1, index2) {
+    [this.#heap[index1], this.#heap[index2]] = [
+      this.#heap[index2],
+      this.#heap[index1],
+    ];
+  }
+}
