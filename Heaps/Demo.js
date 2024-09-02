42ec057fc0bb98e49f095c9346fbf72ddcd2014f
Mohamed Turki <t.momeedo@gmail.com>
2024-09-02 17:22:48 +0100

    Heaps: Insert

--- Heaps/Demo.js
+++ Heaps/Demo.js
@@ -23,4 +23,43 @@ class Heap {
       this.#heap[index1],
     ];
   }
+
+  insert(value) {
+    this.#heap.push(value);
+    let current = this.#heap.length - 1;
+    while (
+      current > 0 &&
+      this.#heap[current] > this.#heap[this.#parent(current)]
+    ) {
+      this.#swap(current, this.#parent(current));
+      current = this.#parent(current);
+    }
+  }
 }
+
+let myHeap = new Heap();
+console.log(myHeap.getHeap());
+
+console.log("\n\n----------------");
+console.log("----------------\n\n");
+
+myHeap.insert(99);
+myHeap.insert(72);
+myHeap.insert(60);
+myHeap.insert(59);
+console.log(myHeap.getHeap());
+
+console.log("\n\n----------------");
+console.log("----------------\n\n");
+
+myHeap.insert(100);
+console.log(myHeap.getHeap());
+
+console.log("\n\n----------------");
+console.log("----------------\n\n");
+
+myHeap.insert(75);
+console.log(myHeap.getHeap());
+
+console.log("\n\n----------------");
+console.log("----------------\n\n");
